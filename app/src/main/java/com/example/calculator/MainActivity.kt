0873f4a45ced23d50d76ad5fef32a45db2fea769
Pujith Kachana <pkachana3@gatech.edu>
2021-03-05 15:53:22 -0500

    Order of Operations

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     var ans = 0.0
@@ -16,8 +17,52 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     fun calculate(view: View) {
         
-        val input = editText.text.toString()
+        var input = editText.text.toString()
+        input = input.replace(" ", "")
+        input = input.toLowerCase()
+        var inp = input.replace("ans", "")
+        var valid = true
+        for(i in inp) {
+            if(!(i.isDigit() || i == '+'|| i == '-'|| i == '*'|| i == '/'|| i == '^')) {
+                valid = false
+            }
+        }
+        if (valid) {
+            ans = calc(input)
+            result.text = "= $ans"
+        } else {
+            result.text = "Enter sumn valid"
+        }
+    }
 
+    fun getNum1(input:String, index:Int):Double {
+        var num = input.substring(0, index)
+        if ("+" in num || "-" in num|| "*" in num|| "/" in num|| "^" in num) {
+            num = calc(num).toString()
+        }
+
+        val num1 = if (num.toLowerCase() == "ans") {
+            ans
+        } else {
+            num.toDouble()
+        }
+        return num1
+    }
+
+    fun getNum2(input:String, index:Int):Double {
+        var num = input.substring(index + 1, input.length)
+        if ("+" in num || "-" in num|| "*" in num|| "/" in num|| "^" in num) {
+            num = calc(num).toString()
+        }
+        val num2 = if (num.toLowerCase() == "ans") {
+            ans
+        } else {
+            num.toDouble()
+        }
+        return num2
+    }
+
+    fun calc(input:String):Double {
         if ("+" in input) {
             val index = input.indexOf("+")
             ans = getNum1(input, index) + getNum2(input, index)
@@ -32,31 +77,10 @@ class MainActivity : AppCompatActivity() {
             ans = getNum1(input, index) / getNum2(input, index)
         } else if ("^" in input) {
             val index = input.indexOf("^")
-            ans = Math.pow(getNum1(input, index), getNum2(input, index))
+            ans = getNum1(input, index).pow(getNum2(input, index))
         } else {
-            result.text = "Invalid input"
-            return
+            ans = input.toDouble()
         }
-        result.text = "= $ans"
-    }
-
-    fun getNum1(input:String, index:Int):Double {
-        val num = input.substring(0, index)
-        val num1 = if (num.toLowerCase() == "ans") {
-            ans
-        } else {
-            num.toDouble()
-        }
-        return num1
-    }
-
-    fun getNum2(input:String, index:Int):Double {
-        val num = input.substring(index + 1, input.length)
-        val num2 = if (num.toLowerCase() == "ans") {
-            ans
-        } else {
-            num.toDouble()
-        }
-        return num2
+        return ans
     }
 }
